061404f75a9df7c3b6da256e343313e46934aa86
Arjun <45332370+iFujin@users.noreply.github.com>
2021-10-22 13:13:35 +0530

    Rock paper Scissor

---
+++ rockpapersissor.py
@@ -0,0 +1,46 @@
+import random
+u=0
+c=0
+for i in range(int(input("how many times"))):
+
+    user=int(input('1.rock\t2.paper\t3.scissor\t'))
+    computer=random.randrange(1,4)
+    if computer==1:
+        print("computer enters: rock")
+        if(user==1):
+            print("tie")
+        elif(user==2):
+            print("User WIN")
+            u=u+1
+        elif(user==3):
+            print("Computer WIN")
+            c=c+1
+    elif(computer==2):
+        print("computer enters: paper")
+        if(user==2):
+            print("tie")
+        elif(user==3):
+            print("User WIN")
+            u=u+1
+        elif(user==1):
+            print("Computer WIN")
+            c=c+1
+    elif(computer==3):
+        print("computer enters: scissor")
+        if(user==3):
+            print("tie")
+        elif(user==1):
+            print("User WIN")
+            u=u+1
+        elif(user==2):
+            print("Computer WIN")
+            c=c+1
+
+print("User points :",u)
+print("Computer points :",c)
+if (u<c):
+    print("Computer Wins")
+elif (u>c):
+    print("User WINS")
+elif (u==c):
+    print('Tie')
